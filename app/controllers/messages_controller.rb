4c3cbc6eca7f88a65e2181a5a511ee4ba421ba2d
rakuiti00 <rakuiti77@gmail.com>
2021-09-07 11:08:37 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,8 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-
     @messages = @room.messages.includes(:user)
+
   end
 
   def create
@@ -16,12 +16,13 @@ class MessagesController < ApplicationController
       @messages = @room.messages.includes(:user)
       render :index   
     end
+    
         
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
